7ad29fc5d257a83844e62904baede53b1d8018ac
Roberto <robeating@outlook.com>
2020-05-02 23:06:15 -0600

    Clase TablaHash agregada
    Se agrego la clase TablaHash la cual ayudara a crear la tabla de dispersion de usuarios.
    Pendiente de agregar la función de buscar en la tabla

---
+++ Proyecto2_EDD/src/Estructuras/TablaHash.java
@@ -0,0 +1,49 @@
+package Estructuras;
+
+import Otras_Clases.Estudiante;
+
+/**
+ *
+ * @author Robert Hernandez
+ */
+public class TablaHash {
+
+    private SLL[] elementos;
+    private int tamanio;
+
+    public TablaHash(int tamanio) {
+        elementos = new SLL[tamanio];
+        this.tamanio = tamanio;
+    }
+
+    public void insert(Estudiante estudiante) {
+        int casilla = funcionHash(estudiante.getCarnet());
+        if (casilla <= 45) {
+            if (elementos[casilla - 1] == null) {
+                elementos[casilla - 1] = new SLL();
+            }
+
+            elementos[casilla - 1].insert(estudiante);
+        }
+    }
+    static int funcionHash(int n) {
+        return (n % 45);
+    }
+
+    public SLL[] getElementos() {
+        return elementos;
+    }
+
+    public void setElementos(SLL[] elementos) {
+        this.elementos = elementos;
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+
+}
